0ae1974161f7887120086029ac86d46061761b33
gegner2015 <xuebin.zhu@gmx.de>
2019-09-07 17:04:35 +0200

    07/09/2019 version4 _ python_lesson_1

--- Python_Grammtik.py
+++ Python_Grammtik.py
@@ -1,6 +1,6 @@
 print ('Hallo Team')
 
-print ('xiaojiu'+' '+'xiaozhu')
+print ('xiaojiu' + 'xiaozhu')
 
 print('xiaomai')
 
@@ -11,3 +11,25 @@ print ('we will successed')
 
 
 print ('1111111111111')
+
+i=50
+
+j='i love you xiaojiu'
+
+m='now'
+
+n='forever'
+
+print(j+' '+m)
+
+if m=='now':
+    print('you are right')
+else:
+    print('you are wrong')
+
+
+
+
+
+
+
