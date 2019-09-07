272e973f14b59210c3511ea264c5a666c32212ce
gegner2015 <xuebin.zhu@gmx.de>
2019-09-07 18:31:51 +0200

    07/09/2019 version5 _ python_lesson_1

--- Python_Grammtik.py
+++ Python_Grammtik.py
@@ -26,6 +26,76 @@ if m=='now':
     print('you are right')
 else:
     print('you are wrong')
+item_one=1
+item_two=2
+
+total1=item_one+item_two+item_two
+
+total2 = item_one + \
+        item_two + \
+        item_two
+
+print(total1)
+
+print(total2)
+
+num= 123456789
+num1=1
+num2=2
+num3=num1+num2
+print(num3)
+str1='1'
+str2='2'
+str3=str1+str2
+print(str3)
+
+str='1234567891231231255555555555511111111111111111111111111111111111111111111111111111111111111111111111111111'  + \
+     '12341254125421561514123123'
+str1='5512312312451231'
+
+print(str+'\n'+str1)
+
+print(str, end="aaaaaaaaaaaaaaaaaaaaaaaaaa")
+print(str1)
+
+print(str+str1)
+
+
+m=29
+if m%2==1:
+    print('i am single')
+
+
+a=1
+a+=3
+
+a+=10 #a=a+10
+
+a-=5
+
+a/=2
+
+a%=2 #a=a%2
+
+a=1 #a=0000 0001
+
+b=60 #b= 0011 1100
+
+a=60 #a=0011 1100
+#a=0111 1000
+
+print(a*2)
+
+print(a<<1)
+
+x=0
+
+y=10
+
+if(x and y ):
+    print('all True')
+else:
+    print('one or all false')
 
 
 
